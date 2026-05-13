7bda6e68f506af2f9a56af6abe3e67ae558c5929
Chrxmee_Midnightt <8gxqw5b2h5@privaterelay.appleid.com>
2026-05-13 17:52:20 -0400

    Fix emojiList handling in EmojiManager

--- chrxmee-bot-maker/managers/EmojiManager.js
+++ chrxmee-bot-maker/managers/EmojiManager.js
@@ -24,4 +24,32 @@ class EmojiManager extends BaseManager {
         const guilds = this.client.storage.getGuilds();
         const g = guilds.find(g => g.id === this.guild.id);
         if (g) {
-            g.emojiList = g.emojiList
+            g.emojiList = g.emojiList || [];
+            g.emojiList.push(emoji);
+            this.client.storage.saveGuilds(guilds);
+            return this.add(emoji);
+        }
+        return null;
+    }
+
+    async delete(id) {
+        const guilds = this.client.storage.getGuilds();
+        const g = guilds.find(g => g.id === this.guild.id);
+        if (g) {
+            g.emojiList = (g.emojiList || []).filter(e => e.id !== id);
+            this.client.storage.saveGuilds(guilds);
+        }
+        this.delete(id);
+        return true;
+    }
+
+    toArray() {
+        return [...this.values()].map(e => ({
+            id: e.id, name: e.name, emoji: e.emoji,
+            url: e.url, animated: e.animated,
+            createdBy: e.createdBy, createdAt: e.createdAt
+        }));
+    }
+}
+
+module.exports = EmojiManager;
