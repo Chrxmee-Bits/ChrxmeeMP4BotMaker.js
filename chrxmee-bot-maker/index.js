fd84b0028565468fd4f74160975b35260bf9118c
Chrxmee_Midnightt <8gxqw5b2h5@privaterelay.appleid.com>
2026-05-13 17:20:02 -0400

    Initialize ChrxmeeBot with utilities and constants

---
+++ chrxmee-bot-maker/index.js
@@ -0,0 +1,8 @@
+const ChrxmeeBot = require('./client/ChrxmeeBot');
+const { Utils, Constants, Permissions } = require('./util');
+
+ChrxmeeBot.Utils = Utils;
+ChrxmeeBot.Constants = Constants;
+ChrxmeeBot.Permissions = Permissions;
+
+module.exports = ChrxmeeBot;
